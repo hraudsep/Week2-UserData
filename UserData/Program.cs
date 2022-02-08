1b369b0be820300efe20fb62f3f7fe1e5b88a5ad
Helen Raudsepp <helen.raudsepp@k-print.ee>
2022-02-08 18:28:11 +0200

    Control Flow

---
+++ UserData/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace UserData
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //programm küsib kasutaja sünniaastat;
+            //programm arvutab kastaja vanust;
+            //programm kuvab kasutaja vanust konsoolist;
+
+            Console.WriteLine("Palun, sisesta oma sünniaasta:");
+            int yearOfBirth = Convert.ToInt32(Console.ReadLine());
+
+            int userAge = 2022 - yearOfBirth;
+
+            //Console.WriteLine("Oled " + userAge + " aastat vana.");
+
+            //string interpolation
+
+            Console.WriteLine($"Oled {userAge} aastat vana.");
+        }
+    }
+}
